3261312d77821b78fa33aced806d511a3bdf11df
Legg <sukhpreets6718@gmail.com>
2024-05-22 15:52:54 -0600

    discontinue use of page action

--- browser-extension/background.ts
+++ browser-extension/background.ts
@@ -1,22 +1,26 @@
 import { urls, extractHostname, extractSurveyId } from "./helper.js";
 import { downloadFiles } from './fetch-ls.js';
-const filter = {
-    urls: urls,
-}
 
-browser.pageAction.onClicked.addListener(function (tab: browser.tabs.Tab) {
+browser.action.onClicked.addListener(function (tab: browser.tabs.Tab) {
     if (!tab.id) return; // This should not happen since the page_action should only be available if applicable tab is open.
+
+    const validServerNames = [
+        "training.vri-research.com",
+        "opinion-insight.com",
+        "research-opinions.com",
+        "opinions-survey.com",
+        "insight-polls.com",
+        "viewpointpoll.com",
+        "p.vri-research.com",
+        "m.vri-research.com",
+    ];
 
     const serverName: string = extractHostname(tab.url);
     const surveyId: string = extractSurveyId(tab.url);
-    if (!serverName || !surveyId) {
+    if (!serverName || !surveyId || !validServerNames.some(name => name===serverName) ) {
         console.error("Wrong tab or switched tab too quickly!");
         return;
     }
 
     downloadFiles(serverName, surveyId);
 });
-
-browser.tabs.onUpdated.addListener((id, changeInfo, tab) => {
-    browser.pageAction.show(tab.id);
-}, filter);
